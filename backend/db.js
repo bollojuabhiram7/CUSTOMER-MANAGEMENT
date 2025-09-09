4ebec97deeee0b2b6f04acb478658211027c783a
bollojuabhiram7 <bollojuabhiram@gmail.com>
2025-09-09 23:50:34 +0530

    change url from local to cloud url

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://root@127.0.0.1:26257/newdb?sslmode=disable',
+  connectionString: "postgresql://abhi:nBuLSBS1nouXUrtt2bnxAQ@sharp-ermine-15772.j77.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+  ssl: {
+    rejectUnauthorized: false, // use Cockroach Cloud SSL certs
+  },
 });
 
 module.exports = pool;
